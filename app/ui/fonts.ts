9fa54531f2be19a80607c56ecf63af00ec653ef1
dMario24 <data.mario24@gmail.com>
2024-11-27 13:00:02 +0900

    add font lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,9 +3,14 @@ import localFont from 'next/font/local'
 
 export const inter = Inter({subsets: ['latin']});
 
-
 export const mFont = localFont({
   src: '../../public/fonts/NanumGothicEco.otf',
   // src: '../../public/fonts/IropkeBatangM.woff',
   display: 'swap',
 })
+
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
